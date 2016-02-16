642a85db3b7d3af88645d720d7ff53c68a1c358f
biswanath_waygum <biswanath@waygum.io>
2016-02-16 15:04:43 +0530

    Cleaned the urls and comments

--- main.go
+++ main.go
@@ -27,31 +27,28 @@ func Version(w http.ResponseWriter, r *http.Request) {
 
 func Login(w http.ResponseWriter, r *http.Request) { 
 
-
-  	// api_url = http://indiadevres3.cloudapp.net:8080/auth/realms/Waygum/protocol/openid-connect/userinfo
-  	// Instantiating the OAuth2 package to exchange the Code for a Token
   	conf := &oauth2.Config{
-  	  ClientID:     "grafana",
-  	  ClientSecret: "10b54f7c-a8ed-4a61-abd7-eb993d12367b",
-  	  RedirectURL: "http://localhost:8090/login", 
-  	  Scopes:       []string{"name","email"},
-  	  Endpoint: oauth2.Endpoint{
-  	    AuthURL:  "http://indiadevres3.cloudapp.net:8080/auth/realms/Waygum/protocol/openid-connect/auth", 
-  	    TokenURL: "http://indiadevres3.cloudapp.net:8080/auth/realms/Waygum/protocol/openid-connect/token" } }
+  	    ClientID:     "grafana",
+  	    ClientSecret: "10b54f7c-a8ed-4a61-abd7-eb993d12367b",
+  	    RedirectURL: "http://localhost:8090/login", 
+  	    Scopes:       []string{"name","email"},
+  	    Endpoint: oauth2.Endpoint{
+  	      AuthURL:  "http://indiadevres3.cloudapp.net:8080/auth/realms/Waygum/protocol/openid-connect/auth", 
+  	      TokenURL: "http://indiadevres3.cloudapp.net:8080/auth/realms/Waygum/protocol/openid-connect/token" } }
 
-  	// Getting the Code that we got from Auth0
+    apiUrl :=  "http://indiadevres3.cloudapp.net:8080/auth/realms/Waygum/protocol/openid-connect/userinfo"
+
   	code := r.URL.Query().Get("code")
-    fmt.Println("reached up to the code ")
 
   	if code == "" {
-  	    http.Redirect(w,r,"http://indiadevres3.cloudapp.net:8080/auth/realms/Waygum/protocol/openid-connect/auth?access_type=online&client_id=grafana&redirect_uri=http%3A%2F%2Flocalhost%3A8090%2Flogin&response_type=code",301)
+        url := conf.AuthCodeURL("")
+        http.Redirect(w, r, url, 301)
         return
   	}
 
   	// Exchanging the code for a token
   	token, err := conf.Exchange(oauth2.NoContext, code)
   	if err != nil {
-        fmt.Println("this is the error")
   	  http.Error(w, err.Error(), http.StatusInternalServerError)
   	  return
   	}
@@ -64,28 +61,20 @@ func Login(w http.ResponseWriter, r *http.Request) {
 
 	client := conf.Client(oauth2.NoContext, token)
 
-    apiUrl :=  "http://indiadevres3.cloudapp.net:8080/auth/realms/Waygum/protocol/openid-connect/userinfo"
-
 	res, err := client.Get(apiUrl)
 	if err != nil {
-		fmt.Println("Error")
+  	    http.Error(w, err.Error(), http.StatusInternalServerError)
 		return 
 	}
 
 	defer res.Body.Close()
 
 	if err = json.NewDecoder(res.Body).Decode(&data); err != nil { 
-		fmt.Println("Some Error")
+  	    http.Error(w, err.Error(), http.StatusInternalServerError)
 		return 
 	}
 
-	fmt.Println("User Name " +  data.Name)
-	fmt.Println("User Id " +  data.Id)
-	fmt.Println("User Email" +  data.Email)
-
-
     io.WriteString(w, data.Email)
-
 }
 
 
